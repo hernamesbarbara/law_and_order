02c059f5fa605d5a5370ea901bcee79ba7ec7fce
Austin Ogilvie <austinogilvie@gmail.com>
2013-06-28 21:01:36 -0400

    cleanup

--- merge_eps_with_recaps.py
+++ merge_eps_with_recaps.py
@@ -7,7 +7,7 @@ import ujson as json
 STOP_WORDS  = list(set(open('./ref/stopwords.txt', 'r').read().split('\n')))
 CRIME_WORDS = list(set(open('./ref/crimes.txt', 'r').read().split('\n')))
 
-pd.options.display.line_width = 200
+pd.options.display.line_width = 500
 pd.options.display.max_columns = 15
 pd.options.display.max_colwidth = 25
 
@@ -18,7 +18,7 @@ def read_file(f = './svu_data/episode_recaps_by_season/recaps_season_1.json'):
     return data, headers
 
 def read_all_files():
-    fmt = './svu_data/episode_recaps_by_season/recaps_season_{0}.json'
+    fmt = './data/svu/episode_recaps_by_season/recaps_season_{0}.json'
     filenames = [fmt.format(i) for i in range(1, 15)]
 
     for i, f in enumerate(filenames):
@@ -27,48 +27,38 @@ def read_all_files():
             df = pd.DataFrame(data, columns=h)
         else:
             data, _ = read_file(f)
-            df = df.append(pd.DataFrame(data, columns=h),ignore_index=True)
+            df = df.append(pd.DataFrame(data, columns=h),
+                ignore_index=True)
 
     order_by = ['nth_season', 'nth_episode']
     df[order_by] = df[order_by].astype(int)
     df = df.sort_index(by=order_by, ascending=[1,0])
-    df = df.reset_index()
+    df = df.reset_index(drop=True)
 
     # i inadvertently reversed the order of the episodes...
     # this fixes it
     df['nth_episode'] = 1
     grouped = df.groupby('nth_season')
     df.nth_episode = grouped.nth_episode.cumsum()
+    df['show'] = 'svu'
 
     return df
 
 
 df = read_all_files()
-episodes = pd.read_csv('./svu_data/law_and_order_episodes.csv')
+episodes = pd.read_csv('./data/combined.csv')
 
-df = pd.merge(episodes, df, how='left',
-              left_on=['season','season_num'],
-              right_on=['nth_season', 'nth_episode'])
+combined = pd.merge(episodes,
+            df, how='left',
+            left_on=['show','nth_season','no_in_season'],
+            right_on=['show', 'nth_season', 'nth_episode'])
 
-df = df.drop(['season', 'index', 'season_num', 'episode_title'], axis=1)
-df = df.rename(columns={'series_num':'nth_episode_in_series'})
 
-reordered = ['nth_season'
-            , 'nth_episode'
-            , 'nth_episode_in_series'
-            , 'title'
-            , 'director'
-            , 'writer'
-            , 'code'
-            , 'viewership'
-            , 'source'
-            , 'corpus_url'
-            , 'corpus']
+duped_columns = ['nth_episode', 'episode_title']
+combined = combined.drop(duped_columns, axis=1)
+combined.corpus[combined.corpus == ''] = None
 
-df = df.ix[:, reordered]
-
-# df.to_csv('./svu_data/law_and_order_svu_with_recaps.txt',
-#     sep='|',
-#     index=False,
-#     encoding='utf-8')
-
+combined.to_csv('./data/episodes_and_recaps.txt',
+    sep='|',
+    index=False,
+    encoding='utf-8')
